738b610ce0ef2b7f1c84876afafba3521225458c
evanjwang <evanjwang>
2024-01-28 19:46:23 +0000

    Added chatbot_chain_test.py
    Step 2.02 of the llm-rag-chatbot demo

---
+++ chatbot_chain_test.py
@@ -0,0 +1,70 @@
+# Databricks notebook source
+# MAGIC %pip install mlflow==2.9.0 lxml==4.9.3 langchain==0.0.344 databricks-vectorsearch==0.22 cloudpickle==2.2.1 databricks-sdk==0.12.0 cloudpickle==2.2.1 pydantic==2.5.2
+# MAGIC %pip install pip mlflow[databricks]==2.9.0
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatDatabricks
+from langchain.schema.output_parser import StrOutputParser
+
+prompt = PromptTemplate(
+  input_variables = ["question"],
+  template = "You are an assistant. Give a short answer to this question: {question}"
+)
+chat_model = ChatDatabricks(endpoint="databricks-mixtral-8x7b-instruct", max_tokens = 500)
+
+chain = (
+  prompt
+  | chat_model
+  | StrOutputParser()
+)
+print(chain.invoke({"question": "What is Spark?"}))
+
+# COMMAND ----------
+
+index_name=f"demo.hackathon.pdf_index_v2"
+host = "https://" + spark.conf.get("spark.databricks.workspaceUrl")
+
+# COMMAND ----------
+
+from databricks.vector_search.client import VectorSearchClient
+from langchain.vectorstores import DatabricksVectorSearch
+from langchain.embeddings import DatabricksEmbeddings
+from langchain.chains import RetrievalQA
+import os
+
+embedding_model = DatabricksEmbeddings(endpoint="databricks-bge-large-en")
+
+def get_retriever(persist_dir: str = None):
+    os.environ["DATABRICKS_HOST"] = host
+    #Get the vector search index
+    vsc = VectorSearchClient(workspace_url=host)
+    vs_index = vsc.get_index(
+        endpoint_name="vector_search_endpoint",
+        index_name=index_name
+    )
+
+    # Create the retriever
+    vectorstore = DatabricksVectorSearch(
+        vs_index, text_column="content", embedding=embedding_model, columns=["path", "content"]
+    )
+    return vectorstore.as_retriever(search_kwargs={'k': 4})
+
+# COMMAND ----------
+
+from langchain.schema.runnable import RunnableLambda
+from operator import itemgetter
+
+#The question is the last entry of the history
+def extract_question(input):
+    return input[-1]["content"]
+
+retriever = get_retriever()
+retrieve_document_chain = (
+    itemgetter("messages") 
+    | RunnableLambda(extract_question)
+    | retriever
+)
+print(retrieve_document_chain.invoke({"messages": [{"role": "user", "content": "What is the effective date for the Colorado Privacy Act?"}]}))
