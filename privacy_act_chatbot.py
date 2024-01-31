3d9b32b52b5fd3ae77b22513d18404cd96ace5e5
Pamela Sanan <pam.sanan@nousot.com>
2024-01-31 17:04:20 -0600

    pull in wip

--- privacy_act_chatbot.py
+++ privacy_act_chatbot.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# MAGIC %pip install transformers==4.30.2 "unstructured[pdf,docx]==0.10.30" langchain==0.0.319 llama-index==0.9.3 databricks-vectorsearch==0.20 pydantic==1.10.9 mlflow==2.9.0 protobuf==3.20.0 openai
+# MAGIC %pip install transformers==4.30.2 "unstructured[pdf,docx]==0.10.30" llama-index==0.9.40 databricks-vectorsearch==0.20 pydantic==1.10.9 mlflow==2.9.0 protobuf==3.20.0 openai==1.10.0 langchain-openai langchain
 # MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
@@ -12,24 +12,21 @@ install_ocr_on_nodes()
 
 # COMMAND ----------
 
-catalog = "demo"
-db = "hackathon"
-volume_folder = f"/Volumes/{catalog}/{db}/privacy_act_docs/*/"
-df = (spark.readStream
-        .format('cloudFiles')
-        .option('cloudFiles.format', 'BINARYFILE')
-        .option("pathGlobFilter", "*.pdf")
-        .load('dbfs:'+volume_folder))
-
-# Write the data as a Delta table
-(df.writeStream
-  .trigger(availableNow=True)
-  .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/raw_docs')
-  .table(f'{catalog}.{db}.pdf_raw').awaitTermination())
-
-# COMMAND ----------
-
-dbutils.secrets.list('dev_demo')
+# MAGIC %md
+# MAGIC catalog = "demo"
+# MAGIC db = "hackathon"
+# MAGIC volume_folder = f"/Volumes/{catalog}/{db}/privacy_act_docs/*/"
+# MAGIC df = (spark.readStream
+# MAGIC         .format('cloudFiles')
+# MAGIC         .option('cloudFiles.format', 'BINARYFILE')
+# MAGIC         .option("pathGlobFilter", "*.pdf")
+# MAGIC         .load('dbfs:'+volume_folder))
+# MAGIC
+# MAGIC # Write the data as a Delta table
+# MAGIC (df.writeStream
+# MAGIC   .trigger(availableNow=True)
+# MAGIC   .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/raw_docs')
+# MAGIC   .table(f'{catalog}.{db}.pdf_raw').awaitTermination())
 
 # COMMAND ----------
 
@@ -49,17 +46,19 @@ def extract_doc_text(x : bytes) -> str:
 
 # COMMAND ----------
 
-import io
-import re
-
-with open("/Volumes/demo/hackathon/privacy_act_docs/Colorado/CPA-regulations.pdf", "rb") as fh:
-    bytes_stream = bytes(fh.read())
-doc = extract_doc_text(bytes_stream)
-print(doc)
+# MAGIC %md
+# MAGIC import io
+# MAGIC import re
+# MAGIC
+# MAGIC with open("/Volumes/demo/hackathon/privacy_act_docs/Colorado/CPA-regulations.pdf", "rb") as fh:
+# MAGIC     bytes_stream = bytes(fh.read())
+# MAGIC doc = extract_doc_text(bytes_stream)
+# MAGIC print(doc)
 
 # COMMAND ----------
 
 from llama_index.langchain_helpers.text_splitter import SentenceSplitter
+from llama_index.node_parser import SemanticSplitterNodeParser
 from llama_index import Document, set_global_tokenizer
 from transformers import AutoTokenizer
 from pyspark.sql.functions import pandas_udf
@@ -69,7 +68,22 @@ import os
 import logging
 from pyspark.sql import functions as F
 import mypy_extensions
+from openai import AzureOpenAI
+from langchain_openai import AzureOpenAIEmbeddings
 
+os.environ["AZURE_OPENAI_API_KEY"] = dbutils.secrets.get(scope='dev_demo', key='azure_openai_api_key')
+os.environ["AZURE_OPENAI_ENDPOINT"] = "https://nous-ue2-openai-sbx-openai.openai.azure.com/"
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment="nous-ue2-openai-sbx-base-deploy-text-embedding-ada-002",
+    openai_api_version="2023-05-15",
+)
+
+client = AzureOpenAI(
+    api_key = dbutils.secrets.get(scope='dev_demo', key='azure_openai_api_key'),
+    api_version = "2023-05-15",
+    azure_endpoint = "https://nous-ue2-openai-sbx-openai.openai.azure.com/",
+    )
 
 # Reduce the arrow batch size as our PDF can be big in memory
 spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", 10)
@@ -78,15 +92,20 @@ os.environ["HF_HOME"] = '/tmp'
 
 @pandas_udf("array<string>")
 def read_as_chunk(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
+    #set embedding model
+    # embed_model = "nous-ue2-openai-sbx-base-deploy-text-embedding-ada-002"
     #set llama2 as tokenizer to match our model size (will stay below BGE 1024 limit)
     set_global_tokenizer(
       AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer", cache_dir = '/tmp')
     )
+    # splitter = SemanticSplitterNodeParser(
+    # buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embeddings
+    # )
     #Sentence splitter from llama_index to split on sentences
-    splitter = SentenceSplitter(chunk_size=50, chunk_overlap=15)
+    base_splitter = SentenceSplitter(chunk_size=500, chunk_overlap=25)
     def extract_and_split(b):
       txt = extract_doc_text(b)
-      nodes = splitter.get_nodes_from_documents([Document(text=txt)])
+      nodes = base_splitter.get_nodes_from_documents([Document(text=txt)])
       logging.info(f"from chunk function: {txt}")
       
       return [n.text for n in nodes]
@@ -96,20 +115,21 @@ def read_as_chunk(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
 
 # COMMAND ----------
 
-from mlflow.deployments import get_deploy_client
-from pprint import pprint
-
-# bge-large-en Foundation models are available using the /serving-endpoints/databricks-bge-large-en/invocations api. 
-deploy_client = get_deploy_client("databricks")
-
-embeddings = deploy_client.predict(endpoint="databricks-bge-large-en", inputs={"input": ["What is Apache Spark?"]})
-pprint(embeddings)
+# MAGIC %md
+# MAGIC from mlflow.deployments import get_deploy_client
+# MAGIC from pprint import pprint
+# MAGIC
+# MAGIC # bge-large-en Foundation models are available using the /serving-endpoints/databricks-bge-large-en/invocations api. 
+# MAGIC deploy_client = get_deploy_client("databricks")
+# MAGIC
+# MAGIC embeddings = deploy_client.predict(endpoint="databricks-bge-large-en", inputs={"input": ["What is Apache Spark?"]})
+# MAGIC pprint(embeddings)
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC --Note that we need to enable Change Data Feed on the table to create the index
-# MAGIC CREATE TABLE IF NOT EXISTS demo.hackathon.databricks_pdf_documentation (
+# MAGIC CREATE TABLE IF NOT EXISTS demo.hackathon.databricks_pdf_documentation_openai (
 # MAGIC   id BIGINT GENERATED BY DEFAULT AS IDENTITY,
 # MAGIC   url STRING,
 # MAGIC   content STRING,
@@ -118,111 +138,55 @@ pprint(embeddings)
 
 # COMMAND ----------
 
-@pandas_udf("array<float>")
-def get_embedding(contents: pd.Series) -> pd.Series:
-    import mlflow.deployments
-    deploy_client = mlflow.deployments.get_deploy_client("databricks")
-    def get_embeddings(batch):
-        #Note: this will fail if an exception is thrown during embedding creation (add try/except if needed) 
-        response = deploy_client.predict(endpoint="databricks-bge-large-en", inputs={"input": batch})
-        return [e['embedding'] for e in response.data]
-
-    # Splitting the contents into batches of 150 items each, since the embedding model takes at most 150 inputs per request.
-    max_batch_size = 150
-    batches = [contents.iloc[i:i + max_batch_size] for i in range(0, len(contents), max_batch_size)]
-
-    # Process each batch and collect the results
-    all_embeddings = []
-    for batch in batches:
-        all_embeddings += get_embeddings(batch.tolist())
-
-    return pd.Series(all_embeddings)
+# MAGIC %md
+# MAGIC @pandas_udf("array<float>")
+# MAGIC def get_embedding(contents: pd.Series) -> pd.Series:
+# MAGIC     import mlflow.deployments
+# MAGIC     deploy_client = mlflow.deployments.get_deploy_client("databricks")
+# MAGIC     def get_embeddings(batch):
+# MAGIC         #Note: this will fail if an exception is thrown during embedding creation (add try/except if needed) 
+# MAGIC         response = deploy_client.predict(endpoint="databricks-bge-large-en", inputs={"input": batch})
+# MAGIC         return [e['embedding'] for e in response.data]
+# MAGIC
+# MAGIC     # Splitting the contents into batches of 150 items each, since the embedding model takes at most 150 inputs per request.
+# MAGIC     max_batch_size = 150
+# MAGIC     batches = [contents.iloc[i:i + max_batch_size] for i in range(0, len(contents), max_batch_size)]
+# MAGIC
+# MAGIC     # Process each batch and collect the results
+# MAGIC     all_embeddings = []
+# MAGIC     for batch in batches:
+# MAGIC         all_embeddings += get_embeddings(batch.tolist())
+# MAGIC
+# MAGIC     return pd.Series(all_embeddings)
 
 # COMMAND ----------
 
-from openai import AzureOpenAI
-
-client = AzureOpenAI(
-    api_key = dbutils.secrets.get(scope='dev_demo', key='azure_openai_api_key'),
-    api_version = "2023-05-15",
-    azure_endpoint = "https://nous-ue2-openai-sbx-openai.openai.azure.com/",
-    
-    )
-
-def open_ai_embeddings(content: str):
+def open_ai_embeddings(contents):
     embed_model = "nous-ue2-openai-sbx-base-deploy-text-embedding-ada-002"
 
     response = client.embeddings.create(
-        input = content,
+        input = contents,
         model = embed_model
     )
 
     return response.data[0].embedding
 
-# openai_udf = F.udf(lambda x: open_ai_embeddings(x, client), StringType())
-
 # COMMAND ----------
 
-# # Reduce the arrow batch size as our PDF can be big in memory
-# spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", 10)
-
-# os.environ["HF_HOME"] = '/tmp'
-
-# set_global_tokenizer(
-#   AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer", cache_dir = '/tmp')
-# )
-# #Sentence splitter from llama_index to split on sentences
-# splitter = SentenceSplitter(chunk_size=500, chunk_overlap=50)
-# def extract_and_split(b):
-#   txt = extract_doc_text(b)
-#   nodes = splitter.get_nodes_from_documents([Document(text=txt)])
-#   return [n.text for n in nodes]
-
-# col = spark.table('demo.hackathon.pdf_raw')
-# pd_col = col.toPandas()
-
-# pd_col["content"].apply(extract_and_split)
-
-volume_folder = f"/Volumes/demo/hackathon/privacy_act_docs/*"
-from pyspark.sql.functions import lit
-
-temp = spark.table('demo.hackathon.databricks_pdf_documentation') \
-      .withColumn("embedding", lit(open_ai_embeddings("content"))) \
-      .selectExpr('url', 'content', 'embedding')
-
-temp.write \
-    .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/pdf_chunk_openai') \
-    .mode("overwrite") \
-    .saveAsTable('demo.hackathon.databricks_pdf_documentation_openai')
-
-# (spark.readStream.table('demo.hackathon.pdf_raw')
-#       .withColumn("content", F.explode(read_as_chunk("content")))
-#       .withColumn("embedding", get_embedding("content"))
-#       .selectExpr('path as url', 'content', 'embedding')
-#   .writeStream
-#     .trigger(availableNow=True)
-#     .option("checkpointLocation", f'dbfs:/Volumes/demo/hackathon/privacy_act_docs/Colorado/checkpoints/pdf_chunk')
-#     .table('demo.hackathon.databricks_pdf_documentation').awaitTermination())
-
-# #Let's also add our documentation web page from the simple demo (make sure you run the quickstart demo first)
-# if table_exists('demo.hackathon.databricks_documentation'):
-#   (spark.readStream.table('databricks_documentation')
-#       .withColumn('embedding', get_embedding("content"))
-#       .select('url', 'content', 'embedding')
-#   .writeStream
-#     .trigger(availableNow=True)
-#     .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/docs_chunks')
-#     .table('databricks_pdf_documentation').awaitTermination())
+# MAGIC %md
+# MAGIC temp = spark.table('demo.hackathon.raw_pdf') \
+# MAGIC       .withColumn("embedding", open_ai_embeddings("content")) \
+# MAGIC       .selectExpr('url', 'content', 'embedding')
+# MAGIC
+# MAGIC temp.write \
+# MAGIC     .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/pdf_chunk_openai') \
+# MAGIC     .mode("overwrite") \
+# MAGIC     .saveAsTable('demo.hackathon.databricks_pdf_documentation_openai')
 
 # COMMAND ----------
 
 from pyspark.sql import functions as F
 import mypy_extensions
-from llama_index.langchain_helpers.text_splitter import SentenceSplitter
-from llama_index import Document, set_global_tokenizer
-from transformers import AutoTokenizer
-from pyspark.sql.functions import pandas_udf
-from typing import Iterator
 import pandas as pd
 import os
 
@@ -231,51 +195,20 @@ import os
 
 # os.environ["HF_HOME"] = '/tmp'
 
-# set_global_tokenizer(
-#   AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer", cache_dir = '/tmp')
-# )
-# #Sentence splitter from llama_index to split on sentences
-# splitter = SentenceSplitter(chunk_size=500, chunk_overlap=50)
-# def extract_and_split(b):
-#   txt = extract_doc_text(b)
-#   nodes = splitter.get_nodes_from_documents([Document(text=txt)])
-#   return [n.text for n in nodes]
-
-# col = spark.table('demo.hackathon.pdf_raw')
-# pd_col = col.toPandas()
-
-# pd_col["content"].apply(extract_and_split)
-
 volume_folder = f"/Volumes/demo/hackathon/privacy_act_docs/*"
 
-temp = spark.table('demo.hackathon.pdf_raw') \
-      .withColumn("content", F.explode(read_as_chunk("content"))) \
-      .withColumn("embedding", get_embedding("content")) \
-      .selectExpr('path as url', 'content', 'embedding')
+temp = (spark.table('demo.hackathon.pdf_raw')
+        .withColumn("content", F.explode(read_as_chunk("content")))
+        .withColumn("embedding", F.lit(open_ai_embeddings("content")))
+        .withColumn("id", F.monotonically_increasing_id())
+        .selectExpr('id', 'path as url', 'content', 'embedding')
+        )
 
-temp.write \
-    .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/pdf_chunk') \
-    .mode("overwrite") \
-    .saveAsTable('demo.hackathon.databricks_pdf_documentation')
-
-# (spark.readStream.table('demo.hackathon.pdf_raw')
-#       .withColumn("content", F.explode(read_as_chunk("content")))
-#       .withColumn("embedding", get_embedding("content"))
-#       .selectExpr('path as url', 'content', 'embedding')
-#   .writeStream
-#     .trigger(availableNow=True)
-#     .option("checkpointLocation", f'dbfs:/Volumes/demo/hackathon/privacy_act_docs/Colorado/checkpoints/pdf_chunk')
-#     .table('demo.hackathon.databricks_pdf_documentation').awaitTermination())
-
-# #Let's also add our documentation web page from the simple demo (make sure you run the quickstart demo first)
-# if table_exists('demo.hackathon.databricks_documentation'):
-#   (spark.readStream.table('databricks_documentation')
-#       .withColumn('embedding', get_embedding("content"))
-#       .select('url', 'content', 'embedding')
-#   .writeStream
-#     .trigger(availableNow=True)
-#     .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/docs_chunks')
-#     .table('databricks_pdf_documentation').awaitTermination())
+(temp.write
+    .option("checkpointLocation", f'dbfs:{volume_folder}/checkpoints/pdf_chunk_openai')
+    .option("overwriteSchema", "true")
+    .mode("overwrite")
+    .saveAsTable('demo.hackathon.databricks_pdf_documentation_openai'))
 
 # COMMAND ----------
 
@@ -345,7 +278,7 @@ temp.write \
 from databricks.vector_search.client import VectorSearchClient
 vsc = VectorSearchClient()
 
-VECTOR_SEARCH_ENDPOINT_NAME = "privacy_vector_search"
+VECTOR_SEARCH_ENDPOINT_NAME = "openai_vector_search"
 
 if VECTOR_SEARCH_ENDPOINT_NAME not in [e['name'] for e in vsc.list_endpoints()['endpoints']]:
     vsc.create_endpoint(name=VECTOR_SEARCH_ENDPOINT_NAME, endpoint_type="STANDARD")
